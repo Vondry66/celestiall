5a576b078ea41d8a91cb1df162e3e3103f7113f5
Ben <b.bax23@hotmail.co.uk>
2022-10-11 15:39:54 +0100

    fix app and home ready for all to pull

--- src/App.js
+++ src/App.js
@@ -1,41 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { useEffect, useState } from "react";
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "./firebase-config";
-import Header from "./components/Header";
-import Home from "./components/Home";
-import "./App.css";
-import { Route,Routes } from "react-router-dom";
-import Photos from "./components/Photos";
-
-function App() {
-  
-;
-
-  return (
-    <div className="App">
-      <Header />
-      <Home />
-      <Routes><Route path = "/photos" element = {Photos}></Route>
-        </Routes>
-    </div>
-=======
-import { useEffect, useState } from 'react';
-import { collection, getDocs } from 'firebase/firestore';
-import { db } from './firebase-config';
 import Header from './components/Header';
 import Home from './components/Home';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
 import Skymap from './components/Skymap';
-=======
-import Header from "./components/Header";
-import Home from "./components/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
-import Skymap from "./components/Skymap";
->>>>>>> 78e9cd0eb60eda989cf39d260bf5e30d29f205cb
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
@@ -52,7 +19,6 @@ function App() {
         </Routes>
       </div>
     </BrowserRouter>
->>>>>>> 407d14b34246b4b22e4f56c59d04ed7650b6942a
   );
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import { getPicture } from "../utils/api"
-import { useEffect } from "react"
-import { useState } from "react"
-
-const Home = ()=>{
-    const[picture,setPicture]=useState([])
-    useEffect(()=>{
-        getPicture().then((pic)=>{
-            console.log(pic)
-            setPicture(pic)
-        })
-    },[])
-=======
 import { getPicture } from "../utils/api";
 import { useEffect } from "react";
 import { useState } from "react";
@@ -23,15 +9,11 @@ const Home = () => {
             setPicture(pic);
         });
     }, []);
->>>>>>> 407d14b34246b4b22e4f56c59d04ed7650b6942a
-
     return (<section>
         <h3>Picture of the day</h3>
         <img className="picture" src={picture.hdurl} alt={picture.title} />
         <p>{picture.title}</p>
         <p>{picture.explanation}</p>
-
-
     </section>);
 };
 
