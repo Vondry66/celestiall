e4eeb106d942c89b201df84af39ea36471726622
Ben <b.bax23@hotmail.co.uk>
2022-10-11 16:45:51 +0100

    add custom search bar for a target, currently not practical in tandem with list search

---
+++ src/components/SearchForTarget.jsx
@@ -0,0 +1,20 @@
+
+const SearchForTarget = ({searchTarget, setSearchTarget}) => {
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        setSearchTarget("")
+    }
+
+
+    return(
+        <section>
+            <form onSubmit={handleChange}>
+                <label htmlFor="search-for-target">Custom search: </label>
+                <input placeholder="Enter target name" id="search-for-target" type="text" value={searchTarget} onChange={e => setSearchTarget(e.target.value)}/>
+            </form>
+        </section>
+    )
+}
+
+export default SearchForTarget;
--- src/components/Skymap.jsx
+++ src/components/Skymap.jsx
@@ -1,23 +1,27 @@
 import { useEffect, useState } from "react";
+import SearchForTarget from "./SearchForTarget";
 import TargetList from "./TargetList";
 
 const Skymap = () => {
 
-    const [target, setTarget] = useState("")
+    const [listTarget, setListTarget] = useState("")
+    const [searchTarget, setSearchTarget] = useState("")
 
     useEffect(() => {
-        if(target){
-            const aladin = window.A.aladin('#aladin-lite-div', { survey: 'P/DSS2/color', fov:5, target: `${target}`})
+        if(listTarget || searchTarget ){
+            let newTarget = listTarget || searchTarget;
+            const aladin = window.A.aladin('#aladin-lite-div', { survey: 'P/DSS2/color', fov:5, target: `${newTarget}`})
         }
-        else if(target === 'None' || !target){
+        else if((listTarget === 'None' && searchTarget === "") || (!listTarget && ! searchTarget)){
             const aladin = window.A.aladin('#aladin-lite-div', { survey: 'P/DSS2/color', fov:60, target: 'trifid'})
         }
-    }, [target])
+    }, [listTarget, searchTarget])
 
 
     return (
         <>
-        <TargetList target={target} setTarget={setTarget}/>
+        <TargetList listTarget={listTarget} setListTarget={setListTarget}/>
+        <SearchForTarget searchTarget={searchTarget} setSearchTarget={setSearchTarget}/>
         <div id='aladin-lite-div' style={{ width: '100%', height: '600px' }} />
         </>
     )
