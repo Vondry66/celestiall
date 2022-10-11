78e9cd0eb60eda989cf39d260bf5e30d29f205cb
vondry66 <vondry66@gmail.com>
2022-10-11 15:22:01 +0100

    updates everything

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
-import { useEffect, useState } from 'react';
-import { collection, getDocs } from 'firebase/firestore';
-import { db } from './firebase-config';
-import Header from './components/Header';
-import Home from './components/Home';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Skymap from './components/Skymap';
+import Header from "./components/Header";
+import Home from "./components/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Skymap from "./components/Skymap";
 
 function App() {
-  const [profile, setProfile] = useState([]);
-  const profileCollectionRef = collection(db, 'Profile');
-  useEffect(() => {
-    const getProfiles = async () => {
-      const data = await getDocs(profileCollectionRef);
-      setProfile(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
-    getProfiles();
-  }, []);
-
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
-        <ol>
-          {profile.map((prof) => {
-            return (
-              <li>
-                {prof.name}
-                {prof.email}
-              </li>
-            );
-          })}
-        </ol>
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/events" element={<Home />} />
