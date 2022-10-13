0d0e97a2aeb3b6337c6bebb884af28684365348e
Alison Rooney <robandroo@Alisons-Air.broadband>
2022-10-13 09:51:12 +0100

    user loggin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
 import logo from "./logo.png";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from "./NavBar";
+import { signInWithGoogle } from "../firebase-config";
 const Header = () => {
   return (
     <div className="Header">
       <header className="App-header">
         <img src={logo} alt="App logo" className="img" />
+        <Button> Create profile </Button>
+        <button className="user"> user</button>
+        <NavBar />
       </header>
-      <NavBar />
     </div>
   );
 };
