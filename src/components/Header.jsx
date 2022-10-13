7751aa4c004caaada38230ca89a0f880d2875413
Alison Rooney <robandroo@Alisons-Air.broadband>
2022-10-13 10:18:47 +0100

    logged in user

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,29 @@
-import logo from "./logo.png";
+import logo from './logo.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from "./NavBar";
-import { signInWithGoogle } from "../firebase-config";
+import NavBar from './NavBar';
 const Header = () => {
   return (
+<<<<<<< HEAD
     <div className="Header">
       <header className="App-header">
         <img src={logo} alt="App logo" className="img" />
         <Button> Create profile </Button>
         <button className="user"> user</button>
         <NavBar />
+=======
+    <div className='Header'>
+      <header className='App-header'>
+        <img src={logo} alt='App logo' className='img' />
+>>>>>>> 494ad2c (logged in user)
       </header>
     </div>
   );
 };
-
 export default Header;
+
+
+
+
+
+
+
