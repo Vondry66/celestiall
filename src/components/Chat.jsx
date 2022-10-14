7026ddf0d6c13b87af18ae6e30e0288c09189791
vondry66 <vondry66@gmail.com>
2022-10-14 09:54:04 +0100

    adds realtime chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { useState,useEffect } from "react";
-import {collection, getDocs, addDoc,doc,deleteDoc} from "firebase/firestore"
+import { useState } from "react";
+import {collection,addDoc,doc,deleteDoc, serverTimestamp,onSnapshot,query,orderBy} from "firebase/firestore"
 import { db } from "../firebase-config";
 
 function Chat(){
@@ -8,35 +8,36 @@ const[newMessage,setNewMessage] = useState("")
 const[messages,setMessages]=useState([])
 const messagesCollRef=collection(db,"messages")
 const SendMessage = async ()=>{
-    await addDoc(messagesCollRef, {text: newMessage})
+    await addDoc(messagesCollRef, {text: newMessage,
+    createdAt: serverTimestamp()})
 }
 const DeleteMessage = async (id, text)=>{
     const messageDoc = doc(db, "messages",id)
     await deleteDoc(messageDoc)
 }
-useEffect(()=>{
-    const getMessages = async()=>{
-        const data = await getDocs(messagesCollRef)
-        console.log(data.docs)
-        setMessages(data.docs.map((doc)=>({...doc.data(),id: doc.id})))
-    }
-    getMessages()
 
-},[])
-    return(
+const q = query(messagesCollRef, orderBy('createdAt'))
+onSnapshot(q, (snapshot)=>{
+ setMessages(snapshot.docs.map((doc)=>({...doc.data(),id:doc.id})))
+})
+    return( 
         <div>
-            {messages.map(({id,text})=>(
+            {messages.map(({id,text,createdAt})=>(
                 <div key={id}>
                     <p>{text}</p>
+                   
                     <button onClick={()=>{DeleteMessage(id)}}>Delete</button>
                     
                     
                 </div>
             ))}
-             <input placeholder="Type your message here..." type="text" onChange={(e)=>{setNewMessage(e.target.value)}}/>
-    <button onClick={SendMessage}>Send Message</button>
+             <input className="message-input" placeholder="Type your message here..." type="text" onChange={(e)=>{setNewMessage(e.target.value)}}/>
+    <button className="send-message" onClick={SendMessage}>Send Message</button>
+    
             
         </div>
+        
     )
 }
+
 export default Chat
