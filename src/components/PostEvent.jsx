c0249f6095773480454c56204d0c3e5881ccc235
Naureen <naureen.imran123@yahoo.com>
2022-10-17 12:14:46 +0100

    post events to database

---
+++ src/components/PostEvent.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { db } from "../firebase-config";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+
+
+function PostEvent() {
+    const [newDate, setNewDate] = useState([]);
+    const [newDescription, setNewDescription] = useState([]);
+    const [newLocation, setNewLocation] = useState([]);
+    const [newEventLink, setNewEventLink] = useState([]);
+
+    const uploadEvent = async (e) => {
+        e.preventDefault();
+        await addDoc(collection(db, "postEvents"), {
+            Date: newDate,
+            Description: newDescription,
+            Location: newLocation,
+            Event_Link: newEventLink
+        });
+        setNewDate('');
+        setNewDescription('');
+        setNewLocation('');
+        setNewEventLink('');
+    };
+
+    return (
+        <div>
+            <form onSubmit={uploadEvent} >
+                <label htmlFor="date"> Date</label>
+                <input onChange={(e) => setNewDate(e.target.value)} value={newDate} name='date'>
+                </input>
+                <label htmlFor="description"> Description</label>
+                <input onChange={(e) => setNewDescription(e.target.value)} value={newDescription} name='description'>
+                </input>
+                <label htmlFor="location"> Location</label>
+                <input onChange={(e) => setNewLocation(e.target.value)} value={newLocation} name='location'>
+                </input>
+                <label htmlFor="eventLink"> Event Link</label>
+                <input onChange={(e) => setNewEventLink(e.target.value)} value={newEventLink} name='eventLink'>
+                </input>
+                <button type="submit">Post Event</button>
+            </form>
+        </div>
+    );
+
+}
+
+export default PostEvent;
