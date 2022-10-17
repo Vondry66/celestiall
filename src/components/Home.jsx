d6776b4c496f047bf10a1f755e2c272613241637
Naureen <naureen.imran123@yahoo.com>
2022-10-17 17:16:59 +0100

    home page carousel

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,17 +2,18 @@ import { getPicture } from "../utils/api";
 import { useEffect } from "react";
 import { useState } from "react";
 import Card from 'react-bootstrap/Card';
+import PhotoCarousel from "./PhotoCarousel";
 const Home = () => {
     const [picture, setPicture] = useState([]);
     useEffect(() => {
         getPicture().then((pic) => {
-            
+
             setPicture(pic);
         });
     }, []);
     return (
         <>
-
+            <Card p-4> <PhotoCarousel /></Card>
             <Card >
                 <Card.Img variant="top" src={picture.hdurl} />
                 <Card.Body>
