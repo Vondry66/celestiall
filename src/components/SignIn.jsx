4dcf34aaa57663130ce4ace16d328f06c3e7e3a6
Naureen <naureen.imran123@yahoo.com>
2022-10-19 17:12:38 +0100

    style google button

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,32 +1,41 @@
 import React, { useEffect } from "react";
 import GoogleButton from "react-google-button";
-import {UserAuth} from '../contexts/AuthContext'
+import { UserAuth } from '../contexts/AuthContext';
 import { useNavigate } from "react-router-dom";
+import { Row, Col, Container } from "react-bootstrap";
 
 const SignIn = () => {
-    const {googleSignIn, user} = UserAuth()
-    const navigate = useNavigate()
-    
+    const { googleSignIn, user } = UserAuth();
+    const navigate = useNavigate();
+
     const handleGoogleSignIn = async () => {
-        try{
-            await googleSignIn()
-        }catch(error){
-            console.log(error)
+        try {
+            await googleSignIn();
+        } catch (error) {
+            console.log(error);
         }
-    }
-useEffect(() => {
- if(user != null) {
-    navigate('/')
- }
-}, [user])
+    };
+    useEffect(() => {
+        if (user != null) {
+            navigate('/');
+        }
+    }, [user]);
 
     return (
         <div>
             <h1>Sign in</h1>
             <div>
-                <GoogleButton onClick={handleGoogleSignIn}/>
+                <Container>
+                    <Row></Row>
+                    <Row></Row>
+                    <Row> <Col></Col>
+                        <Col md='auto'>
+                            <GoogleButton onClick={handleGoogleSignIn} className=' mb-5' size="lg" />
+                        </Col><Col></Col>
+                    </Row>
+                </Container>
             </div>
         </div>
-    )
-}
-export default SignIn
+    );
+};
+export default SignIn;
