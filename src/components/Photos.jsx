841b16caee8c3d979b953b5767232f1bdb129972
vondry66 <vondry66@gmail.com>
2022-10-19 16:52:25 +0100

    LEE IS an  ABSOLUTE LEGEND

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { storage } from "../firebase-config";
+import { db, storage } from "../firebase-config";
 import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
 import { v4 } from "uuid";
 import { useEffect } from "react";
@@ -10,39 +10,73 @@ import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
+import { addDoc,collection,onSnapshot,query} from "firebase/firestore";
 const Photos = () => {
     const [imageUpload, setImageUpload] = useState(null);
     const [imageList, setImageList] = useState([]);
     const imageListRef = ref(storage, "images/");
+    const [desc,setDesc]= useState("")
+  
     const uploadImage = () => {
+        let tempImageURL = "";
         if (imageUpload == null) return;
-        const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+        let imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
         uploadBytes(imageRef, imageUpload).then((snapshot) => {
             getDownloadURL(snapshot.ref).then((url) => {
-
-                setImageList((curr) => [...curr, url]);
-            });
+               
+                tempImageURL=url
+                
+            }).then(()=>{
+                const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+                const picRef = collection(db,"pictures");
+                addDoc(picRef,{
+                    description: desc,
+                    url: tempImageURL
+                })
+                
+            })
         });
     };
-    useEffect(() => {
-        listAll(imageListRef).then((res) => {
-            res.items.forEach((item) => {
-                getDownloadURL(item).then((url) => { setImageList((curr) => [...curr, url]); });
-            });
+   
+   
+    const handleClick = ()=>{
+        uploadImage();
+        // uploadDescription();
+        setDesc('')
+    }
+    
+   useEffect(()=>{
+    const q = query(collection(db, "pictures"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        const newlyPostedPic = [];
+        querySnapshot.forEach((doc) => {
+            newlyPostedPic.push(doc.data());
         });
-    }, []);
+        setImageList(newlyPostedPic);
+        
+    });
+    return()=>unsubscribe()
+   },[])
+   
+
+
     return (
         <Container className="bg">
             <InputGroup className="m-3 p-2">
 
                 <Form.Control className="m-2" onChange={(e) => { setImageUpload(e.target.files[0]); }} type="file" multiple />
-                <Button className="m-2" onClick={uploadImage} >Upload Image</Button>
+                <Form.Control className="m-2" onChange={(e)=> {setDesc(e.target.value)}} type="text" required placeholder="Please describe your picture" value={desc}/>
+                <Button className="m-2" onClick={handleClick} >Upload Image</Button>
             </InputGroup>
             <Row>
+                
                 {imageList.map((url) => {
-                    return <Col md={3} className="m-auto"><Image src={url} fluid className="p-1 m-2 w-100 d-block" thumbnail /></Col>;
+               
+                    return <Col key={url.id} md={3} className="m-auto"><Image src={url.url} fluid className="p-1 m-2 w-100 d-block" thumbnail />
+                    <p>{url.description} </p></Col>;
                 }
                 )}
+                
             </Row>
         </Container>
     );
