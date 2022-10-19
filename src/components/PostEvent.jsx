65621b860087b56df920c38534c9441acb72e948
Naureen <naureen.imran123@yahoo.com>
2022-10-19 12:23:59 +0100

    style skymap

--- src/components/PostEvent.jsx
+++ src/components/PostEvent.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { db } from "../firebase-config";
-import { addDoc, query, collection, onSnapshot } from "firebase/firestore";
+import { addDoc, query, collection, onSnapshot, auth } from "firebase/firestore";
 import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
