4bfe23c5ec58e932cc3e733afbfc06b91d8ef265
Naureen <naureen.imran123@yahoo.com>
2022-10-19 11:01:56 +0100

    post event in a modal window

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import PhotoCarousel from "./PhotoCarousel";
 import Ratio from 'react-bootstrap/Ratio';
+import { Container } from "react-bootstrap";
 const Home = () => {
     const [picture, setPicture] = useState([]);
     useEffect(() => {
@@ -16,28 +17,29 @@ const Home = () => {
     }, []);
     return (
         <>
-        <Card className="bg-black" > <PhotoCarousel /></Card>
-            <Card className="bg-black" >
-                <Row>
-                    <Col className="p-5">
-                        <Ratio aspectRatio={3 / 4}>
-                            <Card.Img
-                                style={{ maxWidth: '100%', maxHeight: 'auto' }}
-                                variant="center" src={picture.hdurl}
-                            />
-                        </Ratio>
-                    </Col>
-                    <Col >
-                        <Card.Body>
-                            <Card.Title className="text-white p-4 mt-4"> {picture.title}</Card.Title>
-                            <Card.Text className="p-4 text-white" >
-                                {picture.explanation}
-                            </Card.Text>
-                        </Card.Body>
-                    </Col>
-                </Row>
-            </Card>
-
+            <Container className='p-5'>
+                <Card className="bg-black" > <PhotoCarousel /></Card>
+                <Card className="bg-black" >
+                    <Row className='p-6'>
+                        <Col className="p-4">
+                            <Ratio aspectRatio={3 / 4}>
+                                <Card.Img className="mt-4"
+                                    style={{ maxWidth: '100%', maxHeight: 'auto' }}
+                                    variant="center" src={picture.hdurl}
+                                />
+                            </Ratio>
+                        </Col>
+                        <Col >
+                            <Card.Body>
+                                <Card.Title className="text-white p-4 mt-4"> {picture.title}</Card.Title>
+                                <Card.Text className="p-4 text-white" >
+                                    {picture.explanation}
+                                </Card.Text>
+                            </Card.Body>
+                        </Col>
+                    </Row>
+                </Card>
+            </Container>
         </>
     );
 };
--- src/components/PostEvent.jsx
+++ src/components/PostEvent.jsx
@@ -1,6 +1,10 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { db } from "../firebase-config";
-import { addDoc, query, collection, getDocs, doc, onSnapshot } from "firebase/firestore";
+import { addDoc, query, collection, onSnapshot } from "firebase/firestore";
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
 
 
 function PostEvent() {
@@ -9,6 +13,11 @@ function PostEvent() {
     const [newLocation, setNewLocation] = useState([]);
     const [newEventLink, setNewEventLink] = useState([]);
     const [eventList, setEventList] = useState([]);
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
 
     const uploadEvent = async (e) => {
         e.preventDefault();
@@ -33,29 +42,73 @@ function PostEvent() {
     });
     return (
         <div>
-            <form onSubmit={uploadEvent} >
-                <label htmlFor="date"> Date</label>
-                <input type='date' onChange={(e) => setNewDate(e.target.value)} value={newDate} name='date' placeholder="Date">
-                </input>
-                <label htmlFor="description"> Description</label>
-                <input onChange={(e) => setNewDescription(e.target.value)} value={newDescription} name='description' placeholder="Description">
-                </input>
-                <label htmlFor="location"> Location</label>
-                <input onChange={(e) => setNewLocation(e.target.value)} value={newLocation} name='location' placeholder="Location">
-                </input>
-                <label htmlFor="eventLink"> Event Link</label>
-                <input onChange={(e) => setNewEventLink(e.target.value)} value={newEventLink} name='eventLink' placeholder="Event Link">
-                </input>
-                <button type="submit">Post Event</button>
-            </form>
+            <>
+                <Button variant="primary" onClick={handleShow}>
+                    Post an Event
+                </Button>
+
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Enter the event details</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form>
+                            <Form.Group className="mb-3" controlId="date">
+                                <Form.Label>Date</Form.Label>
+                                <Form.Control
+                                    type="date"
+                                    autoFocus
+                                    name='date'
+                                    onChange={(e) => setNewDate(e.target.value)} value={newDate}
+                                />
+                            </Form.Group>
+                            <Form.Group
+                                className="mb-3"
+                                controlId="description"
+                            >
+                                <Form.Label>Description</Form.Label>
+                                <Form.Control as="textarea" rows={3}
+                                    onChange={(e) => setNewDescription(e.target.value)} value={newDescription} placeholder="Description of the Event"
+                                />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="location">
+                                <Form.Label>Location</Form.Label>
+                                <Form.Control
+                                    onChange={(e) => setNewLocation(e.target.value)} value={newLocation}
+                                    placeholder="Location"
+                                />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="eventLink">
+                                <Form.Label>Event Link</Form.Label>
+                                <Form.Control
+                                    onChange={(e) => setNewEventLink(e.target.value)} value={newEventLink}
+                                    placeholder="Event Link"
+                                />
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
+                                Post Event
+                            </Button>
+                        </Form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary"
+                            onClick={handleClose}>
+                            Close
+                        </Button>
+
+                    </Modal.Footer>
+                </Modal>
+            </>
 
             <div className="event_body mt-5" >
-                <h3>Upcomming Events</h3>
+
+                <h3>Upcomming Events  </h3>
                 {eventList.map((event) => {
                     return (
 
                         <li key={event.id}>
                             {event.description}
+                            {' '}<Badge bg="secondary">New</Badge>
                         </li>
                     );
                 })}
